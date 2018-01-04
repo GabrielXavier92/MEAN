1729219a5cd17bedf9a72e78ab70b611206402a1
Gabriel Xavier <xavier_gabriel92@hotmail.com>
2018-01-04 11:20:11 -0200

    Adicionado pasta Backend

---
+++ backend/config/server.js
@@ -0,0 +1,14 @@
+const port = 3003
+const bodyParser = require('body-parser')
+const express = require('express')
+const server = express()
+
+//faz com que o body parse interprete a submissao do formulario
+//ele se torna responsavel pela interpretação de todos os dados.
+//para toda requisicao que chegar no servidor, ela sera passada por esse middleware
+server.use(bodyParser.urlencoded({ extended: true })) 
+server.use(bodyParser.json())
+
+server.listen(port, function(){
+    console.log(`BACKEND is runnig on port ${port}.`)
+})
