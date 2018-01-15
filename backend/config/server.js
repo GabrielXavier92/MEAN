1bde4d81b6f4da0d50d6a4ec230512cd3e7c8d8a
gabrielxavier92 <xavier_gabriel92@hotmail.com>
2018-01-15 11:58:50 -0200

    mean sem autenticacao

--- backend/config/server.js
+++ backend/config/server.js
@@ -2,12 +2,16 @@ const port = 3003
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
+const allowCors = require('./cors')
+const queryParser = require('express-query-int')
 
 //faz com que o body parse interprete a submissao do formulario
 //ele se torna responsavel pela interpretação de todos os dados.
 //para toda requisicao que chegar no servidor, ela sera passada por esse middleware
 server.use(bodyParser.urlencoded({ extended: true })) 
 server.use(bodyParser.json())
+server.use(allowCors)
+server.use(queryParser())
 
 server.listen(port, function(){
     console.log(`BACKEND is runnig on port ${port}.`)
